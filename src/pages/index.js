22583cec9a22284b6807bacc42a7983b582e792f
Joseph Kerkhof <joseph.kerkhof@outlook.com>
2020-07-13 17:33:21 -0500

    happy birthday plays automatically

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,19 +3,19 @@ import { Link } from "gatsby"
 
 import Layout from "../components/layout"
 import Image from "../components/image"
+import HappyBirthdaySong from "../audio/happybirthday.mp3"
 import SEO from "../components/seo"
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
+    <audio id="birthday_song" controls autoPlay>
+      <source src={HappyBirthdaySong} />
+    </audio>
+    <p>Welcome to your birthday site! 🎉🥳</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
-    <Link to="/page-2/">Go to page 2</Link> <br />
-    <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
   </Layout>
 )
 
