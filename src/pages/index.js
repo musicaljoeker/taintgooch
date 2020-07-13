b2552eeb631938140b5e301d10ab7dde9aa29b4e
Joseph Kerkhof <joseph.kerkhof@outlook.com>
2020-07-13 17:39:21 -0500

    gooch meme inserted

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,6 +4,7 @@ import { Link } from "gatsby"
 import Layout from "../components/layout"
 import Image from "../components/image"
 import HappyBirthdaySong from "../audio/happybirthday.mp3"
+import GoochMeme from "../images/gooch.mp4"
 import SEO from "../components/seo"
 
 const IndexPage = () => (
@@ -13,8 +14,11 @@ const IndexPage = () => (
       <source src={HappyBirthdaySong} />
     </audio>
     <p>Welcome to your birthday site! 🎉🥳</p>
+    <p>You might say to yourself. Why is this site named so funny? What does it mean to be a gooch and how taint am I? Well, there's one man who can tell you this and a whole lot more.</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
+      <video width="320" height="240" controls autoPlay loop>
+        <source src={GoochMeme} type="video/mp4" />
+      </video>
     </div>
   </Layout>
 )
